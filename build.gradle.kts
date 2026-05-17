ccb2c2ad0b0dc3e80abc9bbf019c64482bd5aaf9
Anton Rukhlov <ya.mr-adam@yandex.com>
2026-05-17 16:27:51 +0500

    add Dockerfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,19 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.util.*
 
-private fun readProperty(key: String, propertiesFile: File = File(rootProject.rootDir, "local.properties")): String {
-    val properties = Properties().apply {
-        propertiesFile.inputStream().use { fis -> load(fis) }
-    }
-
-    return properties.getProperty(key)
-}
+group = "dev.rukhlovar"
 
 plugins {
     kotlin("jvm") version "2.2.20"
+    application
 }
 
-group = "dev.rukhlovar"
-version = "1.0-SNAPSHOT"
+application {
+    mainClass = "dev.rukhlovar.MainKt"
+}
 
 dependencies {
-    testImplementation(kotlin("test"))
     implementation(libs.kord.core)
     implementation(libs.kord.voice)
     implementation(libs.kord.core.voice)
@@ -28,6 +25,14 @@ dependencies {
     implementation(libs.kotlinx.coroutines)
 }
 
+private fun readProperty(key: String, propertiesFile: File = File(rootProject.rootDir, "local.properties")): String {
+    val properties = Properties().apply {
+        propertiesFile.inputStream().use { fis -> load(fis) }
+    }
+
+    return properties.getProperty(key)
+}
+
 tasks.test {
     useJUnitPlatform()
 }
@@ -36,4 +41,15 @@ tasks.withType<JavaExec> {
     systemProperties("bot.token" to readProperty("TOKEN"))
     systemProperties("bot.address" to readProperty("ADDRESS"))
     systemProperties("bot.pass" to readProperty("PASS"))
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    sourceCompatibility = JavaVersion.VERSION_21.toString()
+    targetCompatibility = JavaVersion.VERSION_21.toString()
 }
